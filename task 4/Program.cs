f5737310642b055c8886528c75fa784e3c94ddf1
swikarstha <swikarstha5844@gmail.com>
2025-11-13 18:34:08 +0545

    task 4

---
+++ task 4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace task4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var day = 2;
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+            case 2:
+                Console.WriteLine("Monday");
+                break;
+            case 3:
+                Console.WriteLine("Tuesday");
+                break;
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            case 5:
+                
+                Console.WriteLine("Thursday");
+                break;
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            case 7:
+                Console.WriteLine("Saturday");
+                break;
+            default:
+                Console.WriteLine("Invalid day number");
+                break;
+        }
+    }
+}
